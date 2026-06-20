a5dfa75ce9cc1a815ad6bb9189d22f1aa83e6471
Tasgraphic <arslan.graph@gmail.com>
2026-06-20 23:35:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from database import init_database
 
 # ساخت برنامه
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bot.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # تنظیمات
 app.config.from_object(Config)
